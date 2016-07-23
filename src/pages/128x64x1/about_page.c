1d5c99d9ba9faa744fe0f44a9835b9a1ba2db78c
Leon <cwk545@gmail.com>
2016-07-23 19:21:45 +0800

    Update about_page.c

--- src/pages/128x64x1/about_page.c
+++ src/pages/128x64x1/about_page.c
@@ -19,11 +19,11 @@
 #include "gui/gui.h"
 
 enum {
-    ROW_1_X = 0,
+    ROW_1_X = 8,
     ROW_1_Y = 15,
-    ROW_2_X = 0,
+    ROW_2_X = 8,
     ROW_2_Y = 30,
-    ROW_3_X = 0,
+    ROW_3_X = 4,
     ROW_3_Y = 45
 };
 #endif //OVERRIDE_PLACEMENT
@@ -39,7 +39,7 @@ void PAGE_AboutInit(int page)
     PAGE_ShowHeader(PAGE_GetName(PAGEID_ABOUT));
 
     tempstring_cpy((const char *) _tr("Deviation FW version:"));
-    GUI_CreateLabelBox(&gui->label[0], ROW_1_X, ROW_1_Y, LCD_WIDTH, LINE_HEIGHT, &DEFAULT_FONT, NULL, NULL, "www.deviationtx.com");
-    GUI_CreateLabelBox(&gui->label[1], ROW_2_X, ROW_2_Y, LCD_WIDTH, LINE_HEIGHT, &DEFAULT_FONT, NULL, NULL, tempstring);
+    GUI_CreateLabelBox(&gui->label[0], ROW_1_X, ROW_1_Y, LCD_WIDTH, LINE_HEIGHT, &DEFAULT_FONT, NULL, NULL, _tr("Leon Information Ltd"));
+    GUI_CreateLabelBox(&gui->label[1], ROW_2_X, ROW_2_Y, LCD_WIDTH, LINE_HEIGHT, &DEFAULT_FONT, NULL, NULL, _tr("Owner : Leon`s Clinet"));
     GUI_CreateLabelBox(&gui->label[2], ROW_3_X, ROW_3_Y, LCD_WIDTH, LINE_HEIGHT, &MICRO_FONT, NULL, NULL, _tr_noop(DeviationVersion));
 }
